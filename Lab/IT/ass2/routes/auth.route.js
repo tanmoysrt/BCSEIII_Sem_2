150934800967ca30f3c225a2919725328c8e9796
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-01-25 10:49:13 +0530

    registration completed

--- Lab/IT/ass2/routes/auth.route.js
+++ Lab/IT/ass2/routes/auth.route.js
@@ -19,12 +19,53 @@ router.get("/logout", (req, res) => {
 
 // Registration page
 router.get("/register", (req, res) => {
-    res.render("register");
+    res.render("register", {
+        "isSuccess": false,
+        "isError": false,
+    });
 })
 
 // Handle registration
-router.post("/register", async (req, res) => {
 
+router.post("/register", async (req, res) => {
+    const name = req.body.username;
+    const username =  req.body.username;
+    const password = req.body.password;
+    if(!username || !password || !name) {
+        return res.render("register", {
+            "isSuccess": false,
+            "isError": true,
+            "error": "Please fill in all fields"
+        });
+    }
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username
+        },
+        select: {
+            id: true,
+        }
+    })
+    if(user != null){
+        return res.render("register", {
+            "isSuccess": false,
+            "isError": true,
+            "error": "Username already exists"
+        });
+    }
+    const newUser = await prisma.user.create({
+        data: {
+            name: name,
+            username: username,
+            hashedPassword: password
+        }
+    })
+    // TODO login and redirect to home
+    res.render("register", {
+        "isSuccess": true,
+        "isError": false,
+        "success": "Registration successful"
+    });
 })
 
 
