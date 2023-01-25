97fa66a96c3b966e65712533cc4e7d3aa13171bb
Tanmoy Sarkar <57363826+Tanmoy741127@users.noreply.github.com>
2023-01-25 11:08:31 +0530

    login completed

--- Lab/IT/ass2/routes/auth.route.js
+++ Lab/IT/ass2/routes/auth.route.js
@@ -1,14 +1,66 @@
 const router = require("express").Router();
 const prisma = require("../db").getInstance();
+const Password = require("../utils/password");
+const JWT = require("../utils/jwt");
 
 // login page
-router.get("/login", (req, res) => {
-    res.render("login");
+router.get("/login", async(req, res) => {
+    res.render("login", {
+        "isSuccess": false,
+        "isError": false,
+    });
 })
 
 // Handle login submission
 router.post("/login", async (req, res) => {
-
+    const username =  req.body.username;
+    const password = req.body.password;
+    if(!username || !password) {
+        return res.render("login", {
+            "isSuccess": false,
+            "isError": true,
+            "error": "Please fill in all fields"
+        });
+    }
+    const user = await prisma.user.findFirst({
+        where: {
+            username: username
+        },
+        select: {
+            id: true,
+            name: true,
+            username: true,
+            hashedPassword: true,
+        }
+    })
+    if(user == null){
+        return res.render("login", {
+            "isSuccess": false,
+            "isError": true,
+            "error": "No user exists with this username"
+        });
+    }
+    // Verify hash
+    const passwordVerified = await Password.check(password, user.hashedPassword);
+    if(!passwordVerified) {
+        return res.render("login", {
+            "isSuccess": false,
+            "isError": true,
+            "error": "Incorrect password"
+        });
+    }
+    // Create jwt
+    const generatedToken = JWT.generate({
+        id: user.id,
+        name: user.name,
+        username: user.username,
+    })
+    // Set cookie
+    res.cookie("token", generatedToken, {
+        httpOnly: true,
+    });
+    // Redirect to home
+    res.redirect("/");
 })
 
 // Handle logout
@@ -53,11 +105,12 @@ router.post("/register", async (req, res) => {
             "error": "Username already exists"
         });
     }
+    const generatedHash = await Password.make(password);
     const newUser = await prisma.user.create({
         data: {
             name: name,
             username: username,
-            hashedPassword: password
+            hashedPassword: generatedHash
         }
     })
     // TODO login and redirect to home
--- Lab/IT/ass2/utils/password.js
+++ Lab/IT/ass2/utils/password.js
@@ -4,10 +4,12 @@ class Password {
     static async make(password) {
         const salt = await bcrypt.genSalt(10);
         const hash = await bcrypt.hash(password, salt);
-        console.log(hash);
+        return hash;
     }
     static async check(password, hash) {
         const result = await bcrypt.compare(password, hash);
         return result;
     }
 }
+
+module.exports = Password;
